ce10b83e07468f445a706ff25b15ad7a56490586
kbuczek <krzysztofbuczek@live.com>
2022-01-26 16:53:42 +0100

    2.js

---
+++ fundamentals/2.js
@@ -0,0 +1,12 @@
+//strings are immutable,
+var myStr = "Jello World";
+myStr[0] = "H"; //error
+myStr = "Hello World"; //this works
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  var result = "";
+
+  return result;
+}
+
+console.log(wordBlanks("dog", "big", "ran", "quickly"));

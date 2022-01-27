a6f0f6897c9c8f73b0f693b35c5d555b3464f04d
kbuczek <krzysztofbuczek@live.com>
2022-01-27 17:36:39 +0100

    2.js changes

--- fundamentals/2.js
+++ fundamentals/2.js
@@ -3,10 +3,12 @@ var myStr = "Jello World";
 myStr[0] = "H"; //error
 myStr = "Hello World"; //this works
 
+//functions
 function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
-  var result = "";
+  var result = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb;
 
   return result;
 }
-
 console.log(wordBlanks("dog", "big", "ran", "quickly"));
+
+//arrays

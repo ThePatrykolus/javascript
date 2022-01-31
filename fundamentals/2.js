781d0b910d49f828ef37b2ad72f7a4e52872e80c
kbuczek <krzysztofbuczek@live.com>
2022-01-31 16:46:39 +0100

    3.js

--- fundamentals/2.js
+++ fundamentals/2.js
@@ -12,3 +12,35 @@ function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
 console.log(wordBlanks("dog", "big", "ran", "quickly"));
 
 //arrays
+var ourArray = ["John", 23, 14];
+ourArray[2] = 15;
+ourArray.push("Cow"); //push to array as last element
+
+var newArray = [1, 2, 3];
+var removedFromOurArray = newArray.pop(); //removedFromOurArray now equals 3, and newArray now equals [1,2]
+var removed2FromOurArray = newArray.shift(); //same as pop, but from front. removed2FromArray now equals 1
+
+var array = ["World"];
+array.unshift("Hello"); //array is now ["Hello", "World"]
+
+//global vairable
+var myGlobal = 10;
+
+function fun2() {
+  var output = "";
+  var localVariable = 5; //only visible inside the function
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  console.log(output);
+}
+
+//global vs local
+var myVar = "cat";
+
+function animals() {
+  var myVar = "dog"; //local variable has priority over global variable with the same name
+  return myVar;
+}
+console.log(animals()); //"dog"
+console.log(myVar); //"cat"

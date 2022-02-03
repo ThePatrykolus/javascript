dc0cbdbbeef398afb9d4ef7e8f1efc46b493067f
kbuczek <krzysztofbuczek@live.com>
2022-02-03 16:22:58 +0100

    12.js

--- fundamentals/10.js
+++ fundamentals/10.js
@@ -45,3 +45,41 @@ var y = vector.y;
 var z = vector.z;
 
 const { x: a, y: b, z: c } = vector; //a = 3.6, b = 7.4, c = 6.54
+
+//---
+const AVG_TEMPERATURES = {
+  today: 20,
+  tommorow: 24,
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+
+  const { tommorow: tempOfTomorrow } = avgTemperatures; //destructuring. variable tempOfTommrow has value of tommorow from avgTemperatures object
+  return tempOfTomorrow;
+}
+
+//---
+const AVG_TEMPERATURES = {
+  today: { min: 20, max: 23 },
+  tommorow: { min: 17, max: 19 },
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+
+  const {
+    tommorow: { max: maxTempOfTomorrow, min: minTempOfTommorow },
+  } = avgTemperatures;
+  return maxTempOfTomorrow;
+}
+
+//destructuring assignment
+const [z, x, , y] = [1, 2, 3, 4, 5, 6]; // z = 1, x = 2, y = 4
+
+let a = 8,
+  b = 6;
+() => {
+  "use strict";
+  [a, b] = [b, a]; //a = 6, b = 8
+};

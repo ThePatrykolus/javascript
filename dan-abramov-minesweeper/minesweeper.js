a9b3f9d6232f75dd54f463e561430a9039585385
kbuczek <krzysztofbuczek@live.com>
2022-04-26 16:46:53 +0200

    propagateReveal

--- dan-abramov-minesweeper/minesweeper.js
+++ dan-abramov-minesweeper/minesweeper.js
@@ -1,18 +1,20 @@
-let ROWS = 2;
-let COLS = 2;
+let ROWS = 9;
+let COLS = 9;
 let SIZE = 24;
 const canvas = document.getElementById("canvas");
 
 const cells = new Map();
 const revealedKeys = new Set();
-let map = generateMap(["1-1"]);
+let map = generateMap(generateBombs());
 
 function toKey(row, col) {
   return row + "-" + col;
 }
 
 function fromKey(key) {
-  return key.split("-");
+  return key.split("-").map(Number);
+  // const [row, col] = key.split("-");
+  // return [Number(row), Number(col)];
 }
 
 function createButtons() {
@@ -43,7 +45,10 @@ function updateButtons() {
       if (revealedKeys.has(key)) {
         cell.disabled = true;
         let value = map.get(key);
-        cell.textContent = value.toString();
+        if (value) {
+          cell.textContent = value.toString();
+        }
+
         switch (value) {
           case undefined:
             cell.textContent = "";
@@ -55,6 +60,15 @@ function updateButtons() {
             break;
           case 3:
             cell.style.color = "red";
+            break;
+          case 4:
+            cell.style.color = "darkblue";
+            break;
+          case 5:
+          case 6:
+          case 7:
+          case 8:
+            cell.style.color = "black";
             break;
           case "bomb":
             cell.textContent = "💣";
@@ -73,12 +87,81 @@ function updateButtons() {
 //function updateOneButton(key)
 
 function revealCell(key) {
-  revealedKeys.add(key);
+  // revealedKeys.add(key);
+  propagateReveal(key);
   updateButtons();
+}
+
+function propagateReveal(key) {
+  // const visited = new Set();
+  console.log("propagate", key);
+  revealedKeys.add(key);
+  // visited.add(key);
+
+  // if(map.get(key) === undefined)
+  const isEmpty = !map.has(key);
+  if (isEmpty) {
+    for (let neighbourKey of getNeighbour(key)) {
+      // if (!visited.has(key)) {
+      if (!revealedKeys.has(neighbourKey)) {
+        propagateReveal(key);
+      }
+    }
+  }
+
+  updateButtons();
+}
+
+function isInBounds([row, col]) {
+  if (row < 0 || col < 0) {
+    return false;
+  }
+  if (row >= ROWS || col >= COLS) {
+    return false;
+  }
+  return true;
 }
 
 function getNeighbour(key) {
   const [row, col] = fromKey(key);
+  const neighbourRowCols = [
+    [row - 1, col - 1],
+    [row - 1, col],
+    [row - 1, col + 1],
+    [row, col - 1],
+    [row, col + 1],
+    [row + 1, col - 1],
+    [row + 1, col],
+    [row + 1, col + 1],
+  ];
+  // for (let i = -1; i <= 1; i++) {
+  //   for (let j = -1; j <= 1; j++) {
+  //     if (i !== 0 && j !== 0) {
+  //       neighbourRowCols.push([row + i, col + j]);
+  //     }
+  //   }
+  // }
+  return neighbourRowCols.filter(isInBounds).map(([r, c]) => toKey(r, c));
+}
+
+function generateBombs() {
+  const count = Math.round(Math.sqrt(ROWS * COLS)) + 1;
+  const bombs = [];
+  const allKeys = [];
+
+  for (let i = 0; i < ROWS; i++) {
+    for (let j = 0; j < COLS; j++) {
+      allKeys.push(toKey(i, j));
+    }
+  }
+
+  allKeys.sort(() => {
+    const coinFlip = Math.random() > 0.5;
+    return coinFlip ? 1 : -1;
+  });
+  // arr.slice(2) - returns last 3 elements from array;
+  // arr.slice(0, 2) - returns element 0 and 1;
+  return allKeys.slice(0, count);
 }
 
 function generateMap(seedBombs) {
@@ -99,9 +182,11 @@ function generateMap(seedBombs) {
     map.set(key, "bomb");
     // give all bomb neighbours +1 to value
     for (let neighbourKey of getNeighbour(key)) {
+      console.log(neighbourKey);
       incrementDanger(neighbourKey);
     }
   }
+  console.log(map);
 
   return map;
 }
